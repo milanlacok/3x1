2a036fe2164fa5fc51775a3427ea736f6b5ebe26
Milan Lacok <milan.lacok@navigator.rs>
2021-07-31 23:21:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3x1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i< 999999; i++)
+            {
+                Console.WriteLine($"\ni = {i}");
+
+                DidYouMeanRecursion(i);
+            }
+        }
+
+        static void DidYouMeanRecursion(int i)
+        {
+            if (i == 1)
+                return;
+
+            if (i % 2 == 0)
+            {
+                int x = i / 2;
+                Console.Write($"{x},");
+                DidYouMeanRecursion(x);
+            }
+            else
+            {
+                int x = 3 * i + 1;
+                Console.Write($"{x},");
+                DidYouMeanRecursion(x);
+            }
+        }
+    }
+}
